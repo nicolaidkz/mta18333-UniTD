6db763587e81a196beaa767af7032516b9ba529f
fluthe15 <fluthe15@student.aau.dk>
2018-10-30 12:16:15 +0100

    Game
    Added waypoints for enemy (Working) V.2

--- P3_Product/Assets/Enemy.cs
+++ P3_Product/Assets/Enemy.cs
@@ -19,7 +19,7 @@ public class Enemy : MonoBehaviour {
         Vector3 dir = target.position - transform.position;
         transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
 
-        if(Vector3.Distance(transform.position, target.position) <= 0.2f)
+        if(Vector3.Distance(transform.position, target.position) <= 0.4f)
         {
             GetNextWaypoint();
         }
@@ -27,6 +27,12 @@ public class Enemy : MonoBehaviour {
 
     void GetNextWaypoint()
     {
+        if (wavepointIndex >= Waypoints.points.Length - 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
         wavepointIndex++;
         target = Waypoints.points[wavepointIndex];
     }
