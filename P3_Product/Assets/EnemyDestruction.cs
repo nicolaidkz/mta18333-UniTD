5bf06d284ec710bc969866c24bff1ac3c4bc2c63
Oenger17 <oenger17@student.aau.dk>
2018-11-14 17:33:59 +0100

    Detect Enemy in Range
    Tower can detect enemy in range and undetects them when they exit the trigger.

---
+++ P3_Product/Assets/EnemyDestruction.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDestruction : MonoBehaviour {
+    public delegate void EnemyDelegate(GameObject enemy);
+    public EnemyDelegate enemyDelegate;
+
+    void OnDestroy()
+    {
+        if (enemyDelegate != null)
+        {
+            enemyDelegate(gameObject);
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ P3_Product/Assets/ShootEnemies.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections.Generic;
+public class ShootEnemies : MonoBehaviour {
+    public List<GameObject> enemiesInRange;
+
+    // Use this for initialization
+    void Start () {
+        enemiesInRange = new List<GameObject>();
+
+    }
+
+    void OnEnemyDestroy(GameObject enemy)
+    {
+        enemiesInRange.Remove(enemy);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Enemy"))
+        {
+            enemiesInRange.Add(other.gameObject);
+            EnemyDestruction del =
+                other.gameObject.GetComponent<EnemyDestruction>();
+            del.enemyDelegate += OnEnemyDestroy;
+        }
+    }
+    // 3
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Enemy"))
+        {
+            enemiesInRange.Remove(other.gameObject);
+            EnemyDestruction del =
+                other.gameObject.GetComponent<EnemyDestruction>();
+            del.enemyDelegate -= OnEnemyDestroy;
+        }
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
