71ab4b290f341f886d8faffd048f8be654e4363e
Oenger17 <oenger17@student.aau.dk>
2018-11-14 16:26:33 +0100

    Place Tower
    Place tower within confines of node on mouse click. Checks to see if there is a tower, only if node is empty, can tower be placed.

---
+++ P3_Product/Assets/PlaceTower.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceTower : MonoBehaviour {
+    public GameObject towerPrefab;
+    private GameObject tower;
+
+    private bool CanPlaceTower()
+    {
+        return tower == null;
+    }
+
+    void OnMouseUp()
+    {
+        if (CanPlaceTower())
+        {
+            tower = (GameObject)
+              Instantiate(towerPrefab, transform.position, Quaternion.identity);
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
