1af985e3bf3af141a2c5dcab3aa829a1d6bc62d7
Nicolai <ndanie16@student.aau.dk>
2018-11-14 15:21:35 +0100

    deleted some spaces

--- P3_Product/Assets/Enemy.cs
+++ P3_Product/Assets/Enemy.cs
@@ -2,8 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour {
-
+public class Enemy : MonoBehaviour
+{
     public float speed = 10f;
 
     private Transform target;
