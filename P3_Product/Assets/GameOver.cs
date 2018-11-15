4f0083322e0c028966f4e9fc3bdb17f424457068
nicolai <ndanie16@student.aau.dk>
2018-11-15 23:05:51 +0100

    look in description
    added a base health display that show remaining health out of x
    
    added a wave counter that counts from one to total number of waves
    
    added a text box at the top and bottom this should even out the playing field while still feeding information to the user
    
    added a game over screen that prompts the user to make a decision wheter or not to go to the main menu or to retry the game (only retry implemented)

---
+++ P3_Product/Assets/GameOver.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour {
+
+	void OnEnable () {
+		
+	}
+
+    public void Retry()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Menu() {
+        Debug.Log("Go to main menu");
+    }
+}
---
+++ P3_Product/Assets/WavesUI.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WavesUI : MonoBehaviour {
+
+    public Text wavesText;
+
+    void Update () {
+        for (int i = 1; i <= PlayerStats.ofWaves; i++)
+        {
+            wavesText.text = ((PlayerStats.waves - PlayerStats.waves) + PlayerStats.waveCounter).ToString() + "/" + PlayerStats.ofWaves.ToString() + " Waves";
+        }
+    }
+}
