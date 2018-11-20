4e1c9bfeeadc10f1315070b7a4cbdd68e4bb4040
Oenger17 <Oenger17@student.aau.dk>
2018-11-20 15:25:29 +0100

    2nd Tower
    Added a basic second tower type.
    To place, hold down both mouse buttons on node.
    
    - TODO -
    Fix bullet pathing.

--- P3_Product/Assets/PlaceTower.cs
+++ P3_Product/Assets/PlaceTower.cs
@@ -6,20 +6,28 @@ public class PlaceTower : MonoBehaviour
 {
     private PlayerStats GameMaster;
     public GameObject towerPrefab;
+    public GameObject towerPrefab2;
     private GameObject tower;
     public int cost;
+    public int cost2;
 
     private bool CanPlaceTower()
     {
         return tower == null && GameMaster.Currency >= cost;
     }
 
-    void OnMouseUp()
+    void OnMouseDown()
     {
+        if (Input.GetMouseButton(1) && CanPlaceTower() && GameMaster.Currency >= cost2)
+        {
+            tower = (GameObject) Instantiate(towerPrefab2, transform.position, Quaternion.identity);
+            GameMaster.Currency -= cost2;
+        }
+
+
         if (CanPlaceTower())
         {
-            tower = (GameObject)
-              Instantiate(towerPrefab, transform.position, Quaternion.identity);
+            tower = (GameObject) Instantiate(towerPrefab, transform.position, Quaternion.identity);
               GameMaster.Currency -= cost;
         }
     }
