b71aad0fb80552396634ebd36c051af5bd2ace5d
Nicolai Danielsen <ndanie16@student.aau.dk>
2018-11-24 19:46:16 +0100

    created pause menu

---
+++ P3_Product/Assets/PauseMenu.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+
+    public GameObject ui;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Toggle();
+        }
+	}
+
+    public void Toggle()
+    {
+        // This sets the gameobject to false which is going to disable it
+        ui.SetActive(!ui.activeSelf);
+
+        if (ui.activeSelf)
+        {
+            // pauses the game by setting the frame update time to 0
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            // sets the frame update time back to 1
+            Time.timeScale = 1f;
+        }
+    }
+
+    public void Retry()
+    {
+        Toggle();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Menu()
+    {
+        Debug.Log("Go to menu!");
+    }
+}
