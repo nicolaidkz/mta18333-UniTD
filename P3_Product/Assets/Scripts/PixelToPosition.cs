886e4420a43d99568b1198e80f328b4478c89a72
Exhex <c.kritik@gmail.com>
2018-12-06 19:14:44 +0100

    Added support for grid detection
    results are hardcoded, still pending server implement,
    but all tiles are correctly given x,y coordinates to check against the coordinates of a detected tower!

--- P3_Product/Assets/Scripts/PixelToPosition.cs
+++ P3_Product/Assets/Scripts/PixelToPosition.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class PixelToPosition : MonoBehaviour {
 
+    public GameObject posMaster;
+
 	// Use this for initialization
 	void Start () {
-		
+        posMaster = GameObject.Find("PositionMaster");
 	}
 	
 	// Update is called once per frame
@@ -17,7 +19,27 @@ public class PixelToPosition : MonoBehaviour {
     public string PixelMatch(int x, int y)
     {
         Debug.Log("entered Pixelmatch");
+        var cScript = posMaster.GetComponent<ClientScript>();
+        int[] xValues = new int[120];
+        int[] yValues = new int[120];
 
+        Debug.Log("SANITY IS: " + cScript.arrayPos[0]);
+        int tempX;
+        int tempY;
+        string[] tempCoords;
+        for (int i = 0; i < 92 ; i++)
+        {
+           
+
+
+            tempCoords = cScript.arrayPos[i].Split(","[0]);
+            tempX = int.Parse(tempCoords[0]);
+            tempY = int.Parse(tempCoords[1]);
+            xValues[i] = tempX;
+            yValues[i] = tempY;
+            Debug.Log("Node"+(i+1)+"s X value is " + xValues[i] + " | Y value is " + yValues[i]);
+        }
+        
         if (x < 415 && x > 307 && y < 113 && y > 2)
         {
             // place at A1
